bb749f0ec7e6414f4bc06cdd6b90dcb49d9184d8
travisjungroth <jungroth@gmail.com>
2021-01-18 12:28:37 -0500

    Change sphinx conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,21 +1,9 @@
+import os
+import sys
+
 import sphinx_rtd_theme
 
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+sys.path.insert(0, os.path.abspath('../../graph_examples'))
 
 # -- Project information -----------------------------------------------------
 
@@ -25,7 +13,6 @@ author = 'Travis Jungroth'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -45,7 +32,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 
